b8577ead70e7316a78af70645644ea52be6a2bb7
Johneric0303 <168495545+Johneric0303@users.noreply.github.com>
2025-08-07 12:20:04 +0800

    Add files via upload

---
+++ Act 1 and Act 2.dart
@@ -0,0 +1,7 @@
+  dynamic name = " Alice"; //declaring another variable name for prevent the calling confusion also, the purpose of dynamic is to accept any kind of data inside the variable 
+  print ("Welcome:" + name + "!");
+  
+  var name1 = 45;
+  print ("Your age is: $name1"); //declaring a variable name1 to assign an age of less than 50 to be specific it's equals on 45 then print the output by declaring a print function
+  
+}
